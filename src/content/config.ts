f235f9a1ee91352eaf63c6cf918efb265ed638e7
SStyles93 <samuel.styles93@gmail.com>
2024-09-04 12:33:13 +0200

    Update Website Addition of Blogs Has to be finished

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,8 +14,21 @@ const projectCollection = defineCollection({
 	})
 });
 
+const blogCollection = defineCollection({
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		creationDate: z.coerce.date(),
+		order: z.number(),
+		pinned: z.boolean().default(false),
+		heroImage: image(),
+	})
+});
+
 export const collections = {
 	'projects': projectCollection,
+	'blogs': blogCollection,
 }
 
 export type ProjectSchema = CollectionEntry<"projects">;
+export type BlogSchema = CollectionEntry<"blogs">;
