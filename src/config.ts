2b0a18c04edec199ebab437bf6c0115d827d5793
DESKTOP-B6OVN7J\SamTOUR <samuel.styles93@gmail.com>
2025-09-16 15:40:49 +0200

    Update Analytics TEST V

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,5 @@ export const SITE_TITLE = 'Samuel Styles | Personal Portfolio Website';
 export const SITE_DESCRIPTION = 'Website designed to contain all my work, projects and blogs';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
+
+export const GA_MEASUREMENT_ID = 'G-55XW4V0S7F';
