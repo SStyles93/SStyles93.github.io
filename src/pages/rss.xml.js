3e4cb94b278ded052c27de35350f88128df5b730
DESKTOP-B6OVN7J\SamTOUR <samuel.styles93@gmail.com>
2025-09-16 14:16:36 +0200

    Update RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,13 +3,13 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 
 export async function get(context) {
-  const blog = await getCollection("blog");
+  const blogs = await getCollection("blogs");
   return rss({
 
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: blog.map((post) => ({
+    items: blogs.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
