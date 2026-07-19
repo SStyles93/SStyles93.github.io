a7d99c586631a1d94c05b965da06fc3fd56d8d63
sstyles <samuel.styles93@gmail.com>
2026-07-19 22:58:34 +0200

    install remake-gfm + correction of Shell post

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import partytown from '@astrojs/partytown';
+import remarkGfm from 'remark-gfm';
 
 // Vite plugin to prevent _-prefixed content draft files from being compiled by MDX
 const excludeDrafts = {
@@ -18,6 +19,9 @@ const excludeDrafts = {
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sstyles93.github.io/',
+  markdown: {
+    remarkPlugins: [remarkGfm],
+  },
   integrations: [
     mdx(),
     //sitemap(),
