588197f4bf1a7e0a3f55f81d52eb84abd12fe0f6
sstyles <samuel.styles93@gmail.com>
2026-07-19 22:15:21 +0200

    Addition of Software and tech stack

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -12,6 +12,8 @@ export const skillLanguages: SkillItem[] = [
   { name: "Python",     icon: "devicon-python-plain colored" },
   { name: "HTML",       icon: "devicon-html5-plain colored" },
   { name: "CSS",        icon: "devicon-css3-plain colored" },
+  { name: "JavaScript", icon: "devicon-javascript-plain colored" },
+  { name: "TypeScript", icon: "devicon-typescript-plain colored" },
   { name: "SQL",        icon: "devicon-postgresql-plain colored" },
   { name: "Git",        icon: "devicon-git-plain colored" },
   { name: "Perforce",   svgPath: "m7.386 14.957 2.279-1.316-.576-.333A1.48 1.48 0 0 1 8.334 12c0-.262.073-.915.755-1.308l10.31-5.953a1.49 1.49 0 0 1 1.51 0c.228.13.755.52.755 1.308v11.906c0 .788-.527 1.178-.754 1.308s-.828.393-1.51 0l-2.732-1.577-2.334 1.348 3.899 2.251a3.81 3.81 0 0 0 3.845 0A3.81 3.81 0 0 0 24 17.953V6.047a3.81 3.81 0 0 0-1.922-3.33 3.83 3.83 0 0 0-1.923-.52c-.66 0-1.32.173-1.922.52L7.923 8.67A3.81 3.81 0 0 0 6 12c0 1.17.51 2.234 1.386 2.956zm9.228-5.913-2.279 1.316.576.333c.682.393.755 1.046.755 1.308 0 .263-.073.915-.755 1.308l-10.31 5.954a1.49 1.49 0 0 1-1.51 0 1.48 1.48 0 0 1-.755-1.308V6.047c0-.788.527-1.178.754-1.308s.828-.393 1.51 0l2.732 1.577 2.334-1.348-3.899-2.251a3.81 3.81 0 0 0-3.845 0A3.81 3.81 0 0 0 0 6.047v11.906c0 1.39.72 2.635 1.922 3.33a3.83 3.83 0 0 0 1.923.52c.66 0 1.32-.173 1.922-.52l10.31-5.953A3.81 3.81 0 0 0 18 12c0-1.17-.51-2.234-1.386-2.956" },
@@ -19,6 +21,8 @@ export const skillLanguages: SkillItem[] = [
   { name: "SIMD" },
   { name: "GLSL" },
   { name: "HLSL" },
+  { name: "JSON" },
+  { name: "YAML" },
 ];
 
 export const skillEngines: SkillItem[] = [
@@ -31,6 +35,7 @@ export const skillEngines: SkillItem[] = [
 ];
 
 export const skillSoftware: SkillItem[] = [
+  { name: "Jira",               icon: "devicon-jira-plain colored" },
   { name: "Trello",             icon: "devicon-trello-plain colored" },
   { name: "Miro",       svgPath: "M17.392 0H13.9L17 4.808 10.444 0H6.949l3.102 6.3L3.494 0H0l3.05 8.131L0 24h3.494L10.05 6.985 6.949 24h3.494L17 5.494 13.899 24h3.493L24 3.672 17.392 0z" },
   { name: "Adobe After Effect", icon: "devicon-aftereffects-plain colored" },
